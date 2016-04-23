dd07428ba9bfd8f6061ae08e78c5c44538680732
unknown <173556970@qq.com>
2016-04-23 08:29:49 +0800

    modified:   "Lesson 1/\345\221\250\346\230\223\347\232\2044-21\344\275\234\344\270\232/layout.css" 	modified:   "Lesson 1/\345\221\250\346\230\223\347\232\2044-21\344\275\234\344\270\232/script.js"

--- Lesson 1/周易的4-21作业/script.js
+++ Lesson 1/周易的4-21作业/script.js
@@ -2,13 +2,20 @@
 function getText(){
     var input_text = document.getElementById("input-text");
     var text = input_text.value;
-    return text;
+    if (text.length > 15) {
+        alert("抱歉我记不住15个以上的字请重新输入_(:3 」∠ )_");
+        return "";
+    }
+    else
+        return text;
 }
 
 function createDanMu(text){
     var dan_mu = document.createElement("p");
     dan_mu.setAttribute("class","dan-mu")
     dan_mu.innerText = text;
+    var y_position = Math.random()*400;
+    dan_mu.style.top = y_position+"px";
     return dan_mu;
 }
 
@@ -26,7 +33,13 @@ function handle(){
 function fire(){
     var button = document.getElementById("fire");
     button.onclick = function(){
+        var parent = document.getElementById("dan-mu-wrap");
+        var child = parent.getElementsByTagName("p");
+        if (child[0]) {
+            parent.removeChild(child[0]);
+        }
         handle();
+        document.getElementById("input-text").value = "";
     }
 }
 
