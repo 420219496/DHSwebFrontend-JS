a13ab34315eb518c1495e55b6f903969bcf5b7a4
420219496 <420219496@qq.com>
2016-04-29 10:52:56 +0800

    改进任务

--- Lesson 2/朱大威的井字棋魔改——by周易/animation.js
+++ Lesson 2/朱大威的井字棋魔改——by周易/animation.js
@@ -5,30 +5,27 @@ var yh = document.getElementById("yh");
 ph.style.display = "none";
 var obj = document.getElementById("all");
 var box = obj.getElementsByTagName("div");
-var shu=new Array(0,0,0,0,0,0,0,0,0);
 for (var i = 0; i < box.length; i++) {
-        (function(i){
-        box[i].onclick =function(){
-            changeColor(box[i])
-        }   
+    (function(i) {
+        box[i].onclick = function() {
+            changeColor(box[i]);
+        }
     })(i)
-    box[i].shu=0;
 }
 
 function changeColor(element) {
     if(element.style.backgroundColor == ""){
         if (count == 1) {
-            element.style.backgroundColor = "#c9f";
+            element.style.backgroundColor = "rgb(136, 75, 205)";
             count = 0;
             ph.style.display = "none";
             yh.style.display = "block";
         } else {
-            element.style.backgroundColor = "#ffc";
+            element.style.backgroundColor = "rgb(213, 113, 32)";
             count = 1;
             ph.style.display = "block";
             yh.style.display = "none";
         }
-
     } else {
         return false;
     }
